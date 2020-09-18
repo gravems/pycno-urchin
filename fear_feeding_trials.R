64aeddff9f2244d15009336e1a615ef2ff607365
Ross Whippo <rosspitality@gmail.com>
2020-09-18 12:11:30 -0700

    Updated data, added new treatment column and timepoint, updated graphs

--- fear_feeding_trials.R
+++ fear_feeding_trials.R
@@ -53,55 +53,61 @@ urchin_fear_pycno <- read_csv('Data/urchin_fear_feeding_pycno-trial-1.csv')
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 # mean confetti consumed across all urchins per treatment
+
+# combine trial and bin to prevent combining urchins across trials
+urchin_fear_pycno <- unite(urchin_fear_pycno, 'ID', trial, bin, sep = "_", remove = FALSE)
+
 per_urchin_consumed_total <- urchin_fear_pycno %>%
-  group_by(bin, treatment) %>%
+  group_by(ID, pycno) %>%
   summarise(sum(consumed))
 
 names(per_urchin_consumed_total)[names(per_urchin_consumed_total)=="sum(consumed)"] <- "total_consumed"
 
-ggplot(per_urchin_consumed_total, aes(x = treatment, y = total_consumed, fill = treatment)) +
+
+ggplot(per_urchin_consumed_total, aes(x = pycno, y = total_consumed, fill = pycno)) +
   geom_boxplot() +
   scale_fill_viridis(discrete = TRUE, begin = 0.2, end = 0.9) +
   theme_minimal()
 
-# amount consumed per time
+# amount consumed per timepoint
 
 urchin_timeseries <- urchin_fear_pycno %>%
   unite("datetime", date:time, sep = " ") 
 
 urchin_timeseries$datetime <- as.POSIXlt(urchin_timeseries$datetime)
-ggplot(urchin_timeseries, aes(x = as.POSIXct(datetime), y = consumed, fill = treatment)) +
+
+ggplot(urchin_timeseries, aes(x = as.POSIXct(datetime), y = consumed, fill = pycno)) +
   geom_col() +
   scale_fill_viridis(discrete = TRUE, begin = 0.2, end = 0.9) +
-  facet_wrap(~treatment) 
+  facet_wrap(~pycno) 
 
-# cumulative total consumed by time period
+# cumulative total consumed by time period (only works for one trial)
+
+test <- urchin_timeseries %>%
+  group_by(pycno, ID) %>%
+  mutate(cc = cumsum(consumed))
+
 ggplot(urchin_timeseries %>%
-         group_by(treatment) %>%
+         group_by(pycno, ID) %>%
          mutate(cc = cumsum(consumed)),
-       aes(x = as.POSIXct(datetime), y = cc, color = factor(treatment))) + 
+       aes(x = timepoint, y = cc, color = factor(pycno))) + 
   scale_color_viridis(discrete = TRUE, begin = 0.3, end = 0.9) +
-  geom_line(size = 1) +
   geom_point() +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = -45))
+  geom_smooth(method = "lm") +
+  theme_minimal() 
+
 
 # cumulative total consumed by time period per urchin
-ggplot(urchin_timeseries %>%
-         group_by(bin) %>%
-         mutate(cc = cumsum(consumed)),
-       aes(x = as.POSIXct(datetime), y = cc, color = treatment)) + 
+ggplot(urchin_timeseries, aes(x = as.factor(timepoint), y = consumed, color = pycno, group = ID)) + 
   scale_color_viridis(discrete = TRUE, begin = 0.3, end = 0.9) +
   geom_point() +
   theme_minimal() +
-  geom_smooth(method = lm) +
-  scale_x_datetime(name = "Date") +
-  scale_y_continuous(name = "Cumulative Confetti Consumed") +
+  scale_y_continuous(name = "Confetti Consumed") +
   theme(axis.text.x = element_text(angle = -45)) 
   
-  # total confetti eaten per treatment
+# total confetti eaten per treatment
 urchin_fear_pycno %>%
-  group_by(treatment) %>%
+  group_by(pycno) %>%
   summarise(sum(consumed))
 
 
